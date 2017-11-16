4b0710fa19a78ab7ec76492b2910fece45032d6a
DESKTOP-BLB6I6N\marca <marcantoine_duc@hotmail.com>
2017-11-16 00:34:39 -0500

    Affichage de la table tblLED dans une page php.

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<H1>Hans</H1>
+<?php
+$base = new PDO('mysql:host=localhost; dbname=bursserver_bd', 'root', '');
+$retour = $base->query("SELECT * FROM tblLED;");
+$x =0;
+while ($data = $retour->fetch()){
+    $Ids[$x] = $data['Id'];
+    $Etats[$x] = $data['Etat'];
+    $Emplacements[$x] = $data['Emplacement'];
+    $x++;
+} 
+
+for($y=0;$y<$x;$y++){
+    $message = ('L\'Id est :'.$Ids[$y].' -L\'emplacement est  :'.$Emplacements[$y].'. -La LED est présentement '.EtatLed($Etats[$y]).'.');
+    echo $message;
+}
+
+function EtatLed($etat){
+    $retour = 'allumée';
+    if ($etat){
+        $retour = 'fermée';
+    }
+    return $retour;
+}
+?>
