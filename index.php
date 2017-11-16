d546783ba0213caefc5723afd59d7f90adf79e65
Marc-Antoine <marcantoine_duc@hotmail.com>
2017-11-16 16:05:16 +0000

    le driver php fonctionne, le code n'est pas correcte

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
-$base = new PDO('mysql:host=45.32.219.162; dbname=bursserver_bd', 'root', 'Cell79=68');
-$retour = $base->query("SELECT * FROM tblLED;");
-$x =0;
-while ($data = $retour->fetch()){
-    $Ids[$x] = $data['Id'];
-    $Etats[$x] = $data['Etat'];
-    $Emplacements[$x] = $data['Emplacement'];
-    $x++;
-} 
+echo test;
 
-for($y=0;$y<$x;$y++){
-    $message = ('L\'Id est :'.$Ids[$y].' -L\'emplacement est  :'.$Emplacements[$y].'. -La LED est présentement '.EtatLed($Etats[$y]).'.');
-    echo $message;
+try{
+	$base = new PDO('mysql:host=localhost; dbname=bursserver_bd', 'root', 'Cell79=68');
+	$retour = $base->query("SELECT * FROM tblLED;");
+	$x =0;
+	while ($data = $retour->fetch()){
+	    $Ids[$x] = $data['Id'];
+	    $Etats[$x] = $data['Etat'];
+	    $Emplacements[$x] = $data['Emplacement'];
+	    $x++;
+	}
+	AfficherLED($Ids,$Emplacements,$Etats); 
+} catch(PDOException $e){
+echo $e->getMessage();
 }
-
+function AfficherLED($Ids,$Emplacements,$Etats){
+	for($y=0;$y<$x;$y++){
+	    $message = ('L\'Id est :'.$Ids[$y].' -L\'emplacement est  :'.$Emplacements[$y].'. -La LED est présentement '.EtatLed($Etats[$y]).'.');
+	    echo $message;
+	}
+}
 function EtatLed($etat){
     $retour = 'allumée';
     if ($etat){
